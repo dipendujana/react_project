1f4caccf46afa54bae3625fe941dff6a7cd2eb93
dipendujana5 <your-email@example.com>
2024-07-04 13:00:34 +0530

    react all project

---
+++ 01_react_project/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import "./components/Navigation.jsx"
+import Navigation from "./components/Navigation.jsx";
+import HeroSection from "./components/HeroSection"
+const App = () => {
+  return (
+    <div>
+        <Navigation/>
+        <HeroSection/>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
